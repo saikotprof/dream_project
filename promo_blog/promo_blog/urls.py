e413b6a6c0cb6c114432febaa5c8f8612c1cb267
Asif Arman Saikot <49749089+saikotprof@users.noreply.github.com>
2019-08-06 12:28:51 +0600

    Add files via upload

--- promo_blog/promo_blog/urls.py
+++ promo_blog/promo_blog/urls.py
@@ -7,4 +7,16 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('changed-password/', auth_views.PasswordChangeView.as_view(
+        template_name='users/change-password.html'), name='changed_password'),
+    path('changed-password/done/', auth_views.PasswordChangeDoneView.as_view(
+        template_name='users/changed-password-done.html'), name='password_change_done'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='users/password_reset.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='users/password-reset-done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='users/password_reset_complete.html'), name='password_reset_complete'),
 ]
